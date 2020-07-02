9897e423abbce89de97a3f7efc788ab36edd7b6f
Marc Gonzalez i Carnicer <badukaire@gmail.com>
2020-07-02 16:07:33 +0200

    slice

--- hello.go
+++ hello.go
@@ -3,12 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	var a [2]string
-	a[0] = "Hello"
-	a[1] = "World"
-	fmt.Println(a[0], a[1])
-	fmt.Println(a)
-
 	primes := [6]int{2, 3, 5, 7, 11, 13}
 	fmt.Println(primes)
+	var slice []int = primes[1 : 4]
+	fmt.Println(slice)
+	slice2 := primes[2 : 4]
+	fmt.Println(slice2)
 }
