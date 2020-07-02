567dd2928b4c1c7fbabcac910a6f47cbe1100eba
Marc Gonzalez i Carnicer <badukaire@gmail.com>
2020-07-02 15:55:02 +0200

    struct

--- hello.go
+++ hello.go
@@ -2,15 +2,12 @@ package main
 
 import "fmt"
 
-func main() {
-	i, j := 42, 2701
-
-	p := &i         // point to i
-	fmt.Println(*p) // read i through the pointer
-	*p = 21         // set i through the pointer
-	fmt.Println(i)  // see the new value of i
-
-	p = &j         // point to j
-	*p = *p / 37   // divide j through the pointer
-	fmt.Println(j) // see the new value of j
+type Vertex struct {
+	X int
+	Y int
 }
+
+func main() {
+	fmt.Println(Vertex{1, 2})
+}
+
