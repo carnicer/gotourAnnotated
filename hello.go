3127719205c279bdd9bcfb3470d564a85cb54e59
Marc Gonzalez i Carnicer <badukaire@gmail.com>
2020-07-02 18:54:19 +0200

    clojure

--- hello.go
+++ hello.go
@@ -1,21 +1,24 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
-func compute(fn func(float64, float64) float64) float64 {
-	return fn(3, 4)
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
 }
 
 func main() {
-	hypot := func(x, y float64) float64 {
-		return math.Sqrt(x*x + y*y)
+	pos, neg := adder(), adder()
+	for i := 0; i < 10; i++ {
+		fmt.Println(
+			"i:",
+			i,
+			"--",
+			pos(i),
+			neg(-2*i),
+		)
 	}
-	fmt.Println(hypot(5, 12))
-	fmt.Println(hypot(3, 4))
-
-	fmt.Println(compute(hypot))
-	fmt.Println(compute(math.Pow))
 }
