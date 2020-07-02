c062c1920fcb2cd4f2174bba92911a1d75c6a13b
Marc Gonzalez i Carnicer <badukaire@gmail.com>
2020-07-02 16:11:45 +0200

    change underlying array of slices

--- hello.go
+++ hello.go
@@ -5,8 +5,13 @@ import "fmt"
 func main() {
 	primes := [6]int{2, 3, 5, 7, 11, 13}
 	fmt.Println(primes)
+	fmt.Println("--")
 	var slice []int = primes[1 : 4]
 	fmt.Println(slice)
 	slice2 := primes[2 : 4]
+	fmt.Println(slice2)
+	primes[2] = 0
+	fmt.Println("--")
+	fmt.Println(slice)
 	fmt.Println(slice2)
 }
