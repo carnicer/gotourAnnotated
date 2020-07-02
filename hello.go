06cf8f05cca09311b3e706d5672940b5a14f766e
Marc Gonzalez i Carnicer <badukaire@gmail.com>
2020-07-02 12:45:22 +0200

    run go fmt for formatting all source files

--- hello.go
+++ hello.go
@@ -1,18 +1,17 @@
 package main
 
 import (
-    "fmt"
-    "math/rand"
+	"fmt"
+	"math/rand"
 
-    "example.com/pi/hello/morestrings"
-    "github.com/google/go-cmp/cmp"
+	"example.com/pi/hello/morestrings"
+	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
-    fmt.Println("hello world")
-    fmt.Println("My favorite number is", rand.Intn(10))
+	fmt.Println("hello world")
+	fmt.Println("My favorite number is", rand.Intn(10))
 
-    fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
-    fmt.Println(cmp.Diff("Hello World", "Hello GO"))
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello GO"))
 }
-
