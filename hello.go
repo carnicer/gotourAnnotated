c94e5064b192423fd33464d148394641cf94575c
Marc Gonzalez i Carnicer <badukaire@gmail.com>
2020-07-02 13:12:53 +0200

    use short declarations

--- hello.go
+++ hello.go
@@ -7,6 +7,8 @@ import (
 var i, j int = 1, 2
 
 func main() {
-	var c, python, java = true, false, "no!"
+	c, python, java := true, false, "no!"
 	fmt.Println(i, j, c, python, java)
+	k := 3
+	fmt.Println(k)
 }
