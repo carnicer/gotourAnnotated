d7dae87fa4240b091ebddd9462df1197b2623702
Marc Gonzalez i Carnicer <badukaire@gmail.com>
2020-07-02 12:53:25 +0200

    functions from tour

--- hello.go
+++ hello.go
@@ -2,16 +2,27 @@ package main
 
 import (
 	"fmt"
+
+	/*
 	"math/rand"
 
 	"example.com/pi/hello/morestrings"
 	"github.com/google/go-cmp/cmp"
+	*/
 )
 
+func add(x int, y int) int {
+	return x + y
+}
+
 func main() {
+	fmt.Println(add(42, 11))
+
+	/*
 	fmt.Println("hello world")
 	fmt.Println("My favorite number is", rand.Intn(10))
 
 	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
 	fmt.Println(cmp.Diff("Hello World", "Hello GO"))
+	*/
 }
