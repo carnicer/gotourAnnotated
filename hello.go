14dfc863bdf401948b25a01213a0b7800fb30b7a
Marc Gonzalez i Carnicer <badukaire@gmail.com>
2020-07-02 14:01:54 +0200

    switch with type interface

--- hello.go
+++ hello.go
@@ -2,19 +2,19 @@ package main
 
 import "fmt"
 
-func main() {
-	var i interface{} = "hello"
-
-	s := i.(string)
-	fmt.Println(s)
-
-	s, ok := i.(string)
-	fmt.Println(s, ok)
-
-	f, ok := i.(float64)
-	fmt.Println(f, ok)
-
-	f = i.(float64) // panic
-	fmt.Println(f)
+func do(i interface{}) {
+	switch v := i.(type) {
+	case int:
+		fmt.Printf("int => Twice %v is %v\n", v, v*2)
+	case string:
+		fmt.Printf("string => %q is %v bytes long\n", v, len(v))
+	default:
+		fmt.Printf("default => I don't know about type %T!\n", v)
+	}
 }
 
+func main() {
+	do(21)
+	do("hello")
+	do(true)
+}
