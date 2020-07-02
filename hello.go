848ae6a90b756d7b3cbfe40f6f9b76d37cf389ac
Marc Gonzalez i Carnicer <badukaire@gmail.com>
2020-07-02 12:53:48 +0200

    remove commented code

--- hello.go
+++ hello.go
@@ -2,13 +2,6 @@ package main
 
 import (
 	"fmt"
-
-	/*
-	"math/rand"
-
-	"example.com/pi/hello/morestrings"
-	"github.com/google/go-cmp/cmp"
-	*/
 )
 
 func add(x int, y int) int {
@@ -17,12 +10,4 @@ func add(x int, y int) int {
 
 func main() {
 	fmt.Println(add(42, 11))
-
-	/*
-	fmt.Println("hello world")
-	fmt.Println("My favorite number is", rand.Intn(10))
-
-	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
-	fmt.Println(cmp.Diff("Hello World", "Hello GO"))
-	*/
 }
