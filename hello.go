3bbb7eb6fe4acc4c834e1baf2cba4dfaf5264682
Marc Gonzalez i Carnicer <badukaire@gmail.com>
2020-07-03 13:59:42 +0200

    interfaces

--- hello.go
+++ hello.go
@@ -5,28 +5,41 @@ import (
 	"math"
 )
 
+type Abser interface {
+	Abs() float64
+}
+
+func main() {
+	var a Abser
+	f := MyFloat(-math.Sqrt2)
+
+	a = f  // a MyFloat implements Abser
+	fmt.Printf("abs of f (MyFloat): %v\n", f.Abs())
+	fmt.Printf("abs of a (interface): %v\n", a.Abs())
+
+	v := Vertex{3, 4}
+	a = &v // a *Vertex implements Abser
+	fmt.Printf("abs of v (Vertex*): %v\n", v.Abs())
+	fmt.Printf("abs of a (interface): %v\n", a.Abs())
+
+	// In the following line, v is a Vertex (not *Vertex)
+	// and does NOT implement Abser.
+	//a = v
+}
+
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
 type Vertex struct {
 	X, Y float64
 }
 
-func (v Vertex) Abs() float64 {
+func (v *Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
-
-func AbsFunc(v Vertex) float64 {
-	return math.Sqrt(v.X*v.X + v.Y*v.Y)
-}
-
-func main() {
-	v := Vertex{3, 4}
-	fmt.Println(v.Abs())
-	fmt.Println(AbsFunc(v))
-
-	fmt.Println("--")
-
-	p := &Vertex{4, 3}
-	fmt.Println(p.Abs())
-	fmt.Println((*p).Abs())
-	fmt.Println(AbsFunc(*p))
-}
-
