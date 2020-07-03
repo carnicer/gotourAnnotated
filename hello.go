d17a8532dd49c29729398301d6e1256afbb98a44
Marc Gonzalez i Carnicer <badukaire@gmail.com>
2020-07-03 14:04:17 +0200

    interfaces, not explicit

--- hello.go
+++ hello.go
@@ -1,45 +1,22 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
-type Abser interface {
-	Abs() float64
+type I interface {
+	M()
+}
+
+type Istring struct {
+	S string
+}
+
+// This method means type T implements the interface I,
+// but we don't need to explicitly declare that it does so.
+func (is Istring) M() {
+	fmt.Println(is.S)
 }
 
 func main() {
-	var a Abser
-	f := MyFloat(-math.Sqrt2)
-
-	a = f  // a MyFloat implements Abser
-	fmt.Printf("abs of f (MyFloat): %v\n", f.Abs())
-	fmt.Printf("abs of a (interface): %v\n", a.Abs())
-
-	v := Vertex{3, 4}
-	a = &v // a *Vertex implements Abser
-	fmt.Printf("abs of v (Vertex*): %v\n", v.Abs())
-	fmt.Printf("abs of a (interface): %v\n", a.Abs())
-
-	// In the following line, v is a Vertex (not *Vertex)
-	// and does NOT implement Abser.
-	//a = v
-}
-
-type MyFloat float64
-
-func (f MyFloat) Abs() float64 {
-	if f < 0 {
-		return float64(-f)
-	}
-	return float64(f)
-}
-
-type Vertex struct {
-	X, Y float64
-}
-
-func (v *Vertex) Abs() float64 {
-	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+	var i I = Istring{"hello"}
+	i.M()
 }
