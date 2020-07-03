bb1772014e5d833d30f560dca6b1f83bad87ef4a
Marc Gonzalez i Carnicer <badukaire@gmail.com>
2020-07-03 13:39:50 +0200

    methods, pointers, references (2)

--- hello.go
+++ hello.go
@@ -1,33 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type Vertex struct {
 	X, Y float64
 }
 
-func (v *Vertex) Scale(f float64) {
-	v.X = v.X * f
-	v.Y = v.Y * f
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
-func ScaleFunc(v *Vertex, f float64) {
-	v.X = v.X * f
-	v.Y = v.Y * f
+func AbsFunc(v Vertex) float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
 func main() {
 	v := Vertex{3, 4}
-	fmt.Println(v)
-	v.Scale(2)
-	ScaleFunc(&v, 10)
-	fmt.Println(v)
+	fmt.Println(v.Abs())
+	fmt.Println(AbsFunc(v))
 
 	fmt.Println("--")
 
 	p := &Vertex{4, 3}
-	fmt.Println(p)
-	p.Scale(3)
-	ScaleFunc(p, 8)
-	fmt.Println(p)
+	fmt.Println(p.Abs())
+	fmt.Println((*p).Abs())
+	fmt.Println(AbsFunc(*p))
 }
+
