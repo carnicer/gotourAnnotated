2cbe8abbfc7d63463ee6c4c1efde900d2caf969d
Marc Gonzalez i Carnicer <badukaire@gmail.com>
2020-07-03 15:51:24 +0200

    do it

--- hello.go
+++ hello.go
@@ -5,6 +5,10 @@ import "fmt"
 type IPAddr [4]byte
 
 // TODO: Add a "String() string" method to IPAddr.
+func (ipaddr IPAddr) String() string {
+	return fmt.Sprintf("%d.%d.%d.%d", ipaddr[0], ipaddr[1], ipaddr[2], ipaddr[3])
+}
+
 
 func main() {
 	hosts := map[string]IPAddr{
