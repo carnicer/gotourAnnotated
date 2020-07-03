ad56598a75c79bd1daf6ae200aab5b799305d352
Marc Gonzalez i Carnicer <badukaire@gmail.com>
2020-07-03 13:23:05 +0200

    without pointer, the object is not modified

--- hello.go
+++ hello.go
@@ -18,12 +18,24 @@ func (v *Vertex) Scale(f float64) {
 	v.Y = v.Y * f
 }
 
+func (v Vertex) ScaleNoPointer(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+	fmt.Printf("ScaleNoPointer. size of vertex sides: X=%v, Y=%v\n", v.X, v.Y)
+}
+
 func main() {
 	v := Vertex{3, 4}
 	fmt.Printf("size of vertex sides: X=%v, Y=%v\n", v.X, v.Y)
 	fmt.Printf("size of vertex = %v\n", v.Abs())
+
 	fmt.Println("scaling ...")
 	v.Scale(10)
+	fmt.Printf("size of vertex = %v\n", v.Abs())
+	fmt.Printf("size of vertex sides: X=%v, Y=%v\n", v.X, v.Y)
+
+	fmt.Println("scaling (no pointer) ...")
+	v.ScaleNoPointer(2)
 	fmt.Printf("size of vertex = %v\n", v.Abs())
 	fmt.Printf("size of vertex sides: X=%v, Y=%v\n", v.X, v.Y)
 }
