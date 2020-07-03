b652fa1aef3d10fb74e7cc91bc8b1a57fd1d53af
Marc Gonzalez i Carnicer <badukaire@gmail.com>
2020-07-03 14:09:56 +0200

    more interfaces

--- hello.go
+++ hello.go
@@ -10,13 +10,23 @@ type Istring struct {
 	S string
 }
 
+type Ivector struct {
+	X, Y int
+}
+
 // This method means type T implements the interface I,
 // but we don't need to explicitly declare that it does so.
 func (is Istring) M() {
 	fmt.Println(is.S)
 }
 
+func (iv Ivector) M() {
+	fmt.Println(iv.X, iv.Y)
+}
+
 func main() {
 	var i I = Istring{"hello"}
 	i.M()
+	var v I = Ivector{3, 4}
+	v.M()
 }
