ba282969445bb6695c327967bf19fc22059d9be5
Marc Gonzalez i Carnicer <badukaire@gmail.com>
2020-07-05 15:50:24 +0200

    add sleep and logs to see what's going on actually

--- hello.go
+++ hello.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func fibonacci(n int, c chan int) {
 	x, y := 0, 1
 	for i := 0; i < n; i++ {
+		fmt.Printf("-- i=%d, sleep ...\n", i)
+		time.Sleep(time.Second)
+		fmt.Printf("-- i=%d, channel ...\n", i)
 		c <- x
 		x, y = y, x+y
+		fmt.Printf("-- i=%d, ====\n", i)
 	}
 	close(c)
 }
@@ -16,6 +21,7 @@ func fibonacci(n int, c chan int) {
 func main() {
 	c := make(chan int, 10)
 	go fibonacci(cap(c), c)
+	fmt.Printf("goroutine launched, c size=%d\n", len(c))
 	for i := range c {
 		fmt.Println(i)
 	}
