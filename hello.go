1497480a400edafdf4722ac892f4387a45152c06
Marc Gonzalez i Carnicer <badukaire@gmail.com>
2020-07-05 17:25:36 +0200

    break instead of return

--- hello.go
+++ hello.go
@@ -6,18 +6,23 @@ import (
 )
 
 func main() {
+	boomSet := false
+	ticks := 0
 	tick := time.Tick(100 * time.Millisecond)
 	boom := time.After(500 * time.Millisecond)
 	for {
 		select {
 		case <-tick:
+			ticks++
 			fmt.Println("tick.")
 		case <-boom:
 			fmt.Println("BOOM!")
-			return
+			boomSet = true
 		default:
 			fmt.Println("    .")
 			time.Sleep(50 * time.Millisecond)
 		}
+		if boomSet { break }
 	}
+	fmt.Println(ticks)
 }
