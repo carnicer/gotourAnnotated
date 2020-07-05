2146f66152183a33c0081b13dd977cf0afec1a5d
Marc Gonzalez i Carnicer <badukaire@gmail.com>
2020-07-05 16:43:55 +0200

    sleeps and logs

--- hello.go
+++ hello.go
@@ -1,24 +1,35 @@
 package main
 
 import "fmt"
+import "time"
 
 func fibonacci(c, quit chan int) {
 	x, y := 0, 1
 	for {
 		select {
 		case c <- x:
+			fmt.Printf("fibonacci: number sent: %d\n", x)
 			x, y = y, x+y
+			fmt.Printf("fibonacci: now x=%d, y=%d\n", x, y)
 		case <-quit:
-			fmt.Println("quit")
+			fmt.Println("fibonacci: quit")
 			return
 		}
 	}
 }
 
 func feeder(c, quit chan int) {
+		var fibonum int
 		for i := 0; i < 10; i++ {
-			fmt.Println(<-c)
+			fmt.Println("feeder: sleep ...")
+			time.Sleep(2 * time.Second)
+			fmt.Println("feeder: wait ...")
+			fibonum = <-c
+			fmt.Printf("feeder: fibonacci => %d\n", fibonum)
 		}
+		fmt.Println("feeder: sleep before QUIT ...\n")
+		time.Sleep(time.Second)
+		fmt.Println("feeder: send QUIT via its channel\n")
 		quit <- 0
 }
 
