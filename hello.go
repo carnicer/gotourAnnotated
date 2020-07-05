35d24ef0a7c7ea8f83a7088f13f9c914cc0424bb
Marc Gonzalez i Carnicer <badukaire@gmail.com>
2020-07-05 17:56:02 +0200

    mutex, with 1000 goroutines

--- hello.go
+++ hello.go
@@ -2,27 +2,38 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
+	//"time"
 )
 
+// SafeCounter is safe to use concurrently.
+type SafeCounter struct {
+	v   map[string]int
+	mux sync.Mutex
+}
+
+// Inc increments the counter for the given key.
+func (c *SafeCounter) Inc(key string) {
+	c.mux.Lock()
+	// Lock so only one goroutine at a time can access the map c.v.
+	c.v[key]++
+	c.mux.Unlock()
+}
+
+// Value returns the current value of the counter for the given key.
+func (c *SafeCounter) Value(key string) int {
+	c.mux.Lock()
+	// Lock so only one goroutine at a time can access the map c.v.
+	defer c.mux.Unlock()
+	return c.v[key]
+}
+
 func main() {
-	boomSet := false
-	ticks := 0
-	tick := time.Tick(100 * time.Millisecond)
-	boom := time.After(500 * time.Millisecond)
-	for {
-		select {
-		case <-tick:
-			ticks++
-			fmt.Println("tick.")
-		case <-boom:
-			fmt.Println("BOOM!")
-			boomSet = true
-		default:
-			fmt.Println("    .")
-			time.Sleep(50 * time.Millisecond)
-		}
-		if boomSet { break }
+	c := SafeCounter{v: make(map[string]int)}
+	for i := 0; i < 1000; i++ {
+		go c.Inc("somekey")
 	}
-	fmt.Println(ticks)
+
+	time.Sleep(time.Second)
+	fmt.Println(c.Value("somekey"))
 }
